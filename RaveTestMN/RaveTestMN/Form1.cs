6e7e004d005c5fa8557cb8d8c02b9c27cc04dfd7
nooranimurtuza <murtuza.noorani@northgateps.com>
2017-06-01 19:31:22 +0530

    d

--- RaveTestMN/RaveTestMN/Form1.cs
+++ RaveTestMN/RaveTestMN/Form1.cs
@@ -20,7 +20,8 @@ namespace RaveTestMN
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hi RaveTestMN");
-            MessageBox.Sow("Hi RaveTestMN");
+            MessageBox.Show("Hi RaveTestMN 2nd");
+            MessageBox.Show("Hi RaveTestMN 3rd");
         }
     }
 }
